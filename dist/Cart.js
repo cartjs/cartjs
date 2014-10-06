df74752e0c9f3b3ba8525f6a22e9dc85be13c72a
Andrey Knupp Vital <andreykvital@gmail.com>
2014-10-06 19:11:14 -0300

    definindo os métodos explicitamente no prototype.

--- dist/Cart.js
+++ dist/Cart.js
@@ -3,22 +3,22 @@ var ArrayProductStorage = require ('./Storage/ArrayProductStorage');
 
 function Cart (storage) {
   this.productStorage = storage || new ArrayProductStorage();
+}
 
-  this.get = function (rules) {
-    return this.productStorage.get(rules);
-  };
+Cart.prototype.get = function (rules) {
+  return this.productStorage.get(rules);
+};
 
-  this.list = function () {
-    return this.productStorage.getAll();
-  };
+Cart.prototype.list = function () {
+  return this.productStorage.getAll();
+};
 
-  this.add = function (item) {
-    this.productStorage.add(item);
-  };
+Cart.prototype.add = function (item) {
+  this.productStorage.add(item);
+};
 
-  this.remove = function (item) {
-    this.productStorage.remove(item);
-  };
+Cart.prototype.remove = function (item) {
+  this.productStorage.remove(item);
 };
 
 module.exports = Cart;

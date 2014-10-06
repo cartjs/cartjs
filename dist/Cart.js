e24443ce2253250fb2ad6f8c2f274f600147660c
Andrey Knupp Vital <andreykvital@gmail.com>
2014-10-06 19:01:31 -0300

    refatorando o source.

--- dist/Cart.js
+++ dist/Cart.js
@@ -1,9 +1,6 @@
 var Item = require ('./Item.js');
 
 function Cart (options) {
-  this.count = 0;
-  this.items = [];
-
   this.productStorage = options.engine;
 
   this.get = function (rules) {
@@ -15,25 +12,12 @@ function Cart (options) {
   };
 
   this.add = function (item) {
-    this.items.push(item);
     this.productStorage.add(item);
   };
 
   this.remove = function (item) {
     this.productStorage.remove(item);
   };
-
-  this.flush = function () {
-    this.items = [];
-  };
-
-  this.isEmpty = function () {
-    return 0 === this.getItemsCount();
-  };
-};
-
-Cart.prototype.getItemsCount = function () {
-  return this.count;
 };
 
 module.exports = Cart;
--- dist/engines/Generic.js
+++ dist/engines/Generic.js
@@ -5,14 +5,12 @@ function Generic () {
   this.items = [];
 };
 
-Generic.prototype = new ProductStorage;
+Generic.prototype = new ProductStorage();
 
-Generic.prototype.get = function (rules) {
-  if (_.where(this.items, rules).length > 1) {
-    return _.where(this.items, rules);
-  };
-
-  return _.findWhere(this.items, rules);
+Generic.prototype.get = function (id) {
+  return _.findWhere(this.items, {
+    id: parseInt(id)
+  });
 };
 
 Generic.prototype.getAll = function () {
@@ -23,8 +21,11 @@ Generic.prototype.add = function (item) {
   this.items.push(item);
 };
 
-Generic.prototype.remove = function (rules) {
-  var items = this.get(rules);
+Generic.prototype.remove = function (id) {
+  this.items = _.without(
+    this.items,
+    this.get(id)
+  );
 };
 
 module.exports = Generic;
